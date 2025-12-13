90d8aea58a4cdf77b38d96819029786c496091d8
Takumin <akahori.t.24kdgn@gmail.com>
2025-12-14 00:05:57 +0900

    ユーザー認証機能の追加とローカライズ設定の実装

--- Program.cs
+++ Program.cs
@@ -19,7 +19,16 @@ builder.Services
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddLocalization( options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddViewLocalization();
+
 var app = builder.Build();
+
+var supportedCultures = new[] { "ja-JP" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    // ★ localizationOptions ではなく supportedCultures を使用
+    .AddSupportedUICultures(supportedCultures);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
